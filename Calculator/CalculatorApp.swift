1ddb67f9580177696561bd1782ac7d3243dadb68
MaksimFomenko <maksim.fomenko.1996@gmail.com>
2024-02-21 11:06:21 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Maksim on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
